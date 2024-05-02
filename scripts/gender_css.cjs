ffe79df701df62782f20e5616f97d16b2fd65e0e
CNChestnut <liyufeng0628@outlook.com>
2024-05-02 14:14:22 +0800

    Add map file

--- scripts/gender_css.cjs
+++ scripts/gender_css.cjs
@@ -5,6 +5,7 @@ json = fs.readFileSync(__dirname + "\\..\\info.json");
 let info = JSON.parse(json).data;
 
 let css = ``;
+let map = [];
 let files = [];
 
 for (let font of info) {
@@ -18,9 +19,15 @@ for (let font of info) {
     font-family: "${file.font_name}";
 }
 `;
+    map.push({
+      font_name: file.font_name,
+      font_family_name: font.font_family_name,
+      font_css_tag_name: `hoyo-glyphs-${file.font_name}`
+    });
     // Delete the filetype value
     // https://github.com/SpeedyOrc-C/HoYo-Glyphs/issues/18
   }
 }
 
 fs.writeFileSync(__dirname + "/../index.css", css);
+fs.writeFileSync(__dirname + "/../map.json", JSON.stringify(map, null, 4));
